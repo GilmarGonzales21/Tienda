8b30a3bbd02e6ca0ed75c4a977e26738458a3925
GilmarGonzales21 <gilmarjoegonzaleshernandez5@gmail.com>
2025-11-06 10:21:01 -0500

    Main

--- Tienda/src/tienda/Tienda.java
+++ Tienda/src/tienda/Tienda.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tienda;
-
+import java.util.Scanner;
 /**
  *
  * @author Gilmar Gonzales
@@ -14,7 +14,23 @@ public class Tienda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String nombre, producto;
+        int cantidad;
+        System.out.println("Ingrese su nombre: ");
+        nombre = sc.nextLine();
+        System.out.println("Ingrese el producto (cemento sol, cemento apu, cemento holcim: ");
+        producto = sc.nextLine();
+        System.out.println("Ingrese la cantidad que desea: ");
+        cantidad = sc.nextInt();
+        
+        if (!producto.equalsIgnoreCase("cemento sol") ||!producto.equalsIgnoreCase("cemento apu")||!producto.equalsIgnoreCase("cemento holcim")) {
+            System.out.println("No disponible");
+        }
+        else {
+            PedidoFacade pFacade = new PedidoFacade();
+            pFacade.registrarPedido(nombre, producto, cantidad);
+        }
     }
     
 }
