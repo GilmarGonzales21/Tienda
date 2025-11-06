ff866e7bfb02ea43332b95d0aff51f9ae2b306b2
GilmarGonzales21 <gilmarjoegonzaleshernandez5@gmail.com>
2025-11-06 09:23:32 -0500

    Update calculoImpuestos.java

--- Tienda/src/tienda/calculoImpuestos.java
+++ Tienda/src/tienda/calculoImpuestos.java
@@ -9,5 +9,14 @@ package tienda;
  * @author Gilmar Gonzales
  */
 public class calculoImpuestos {
+    public double calcularSubTotal(String producto, int cantidad){
+        validacionStock validacion = new validacionStock();
+        double precio = validacion.obtenerPrecio(producto);
+        return precio * cantidad;
+    }
+    
+    public double calcularIGV(double subtotal){
+        return subtotal * 0.18;
+    }
     
 }
