9bc00fbf7394b8bc0890ac60359627f7d9e034c1
GilmarGonzales21 <gilmarjoegonzaleshernandez5@gmail.com>
2025-11-06 10:12:10 -0500

    Update PedidoFacade.java

--- Tienda/src/tienda/PedidoFacade.java
+++ Tienda/src/tienda/PedidoFacade.java
@@ -9,5 +9,32 @@ package tienda;
  * @author Gilmar Gonzales
  */
 public class PedidoFacade {
+    private validacionStock vStock;
+    private calculoImpuestos cImpuestos;
+    private registroPedido rPedido;
+    private generarComprobante gComprobante;
+    private FacturaService fService;
+    
+    public PedidoFacade(){
+        vStock = new validacionStock();
+        cImpuestos = new calculoImpuestos();
+        rPedido = new registroPedido();
+        gComprobante = new generarComprobante();
+        fService = new FacturaAdapter();
+    }
+    
+    public void registrarPedido(String cliente, String producto, int cantidad){
+        if (vStock.validarStock(producto, cantidad)) {
+            double subtotal = cImpuestos.calcularSubTotal(producto, cantidad);
+            double igv = cImpuestos.calcularIGV(subtotal);
+            double total = subtotal + igv;
+            
+            rPedido.registrarPedido(cliente, producto, cantidad, subtotal, igv, total);
+            gComprobante.generarComprobante(cliente, producto, subtotal, igv, total);
+            fService.generarFactura(cliente, producto, subtotal, igv, total);
+        } else {
+            System.out.println("Stock insuficiente para "+ producto);
+        }
+    }
     
 }
