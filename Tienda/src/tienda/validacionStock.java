16a789c805adad9fede066572bc860ec0d55a79f
GilmarGonzales21 <gilmarjoegonzaleshernandez5@gmail.com>
2025-11-06 09:21:29 -0500

    Creacion de clase producto
    cree una clase producto para poder hacer una validacion externa, ademas de la funcionalidad de validar el stock

--- Tienda/src/tienda/validacionStock.java
+++ Tienda/src/tienda/validacionStock.java
@@ -9,5 +9,38 @@ package tienda;
  * @author Gilmar Gonzales
  */
 public class validacionStock {
+    private Producto cementoSol = new Producto("Cemento Sol", 30, 15);
+    private Producto cementoApu = new Producto("Cemento Apu", 27.5, 20);
+    private Producto cementoHolcim = new Producto("Cemento Holcim", 29, 5);
+    
+    public boolean validarStock(String producto, int cantidad){
+        switch (producto.toLowerCase()) {
+            case "cemento sol":
+                return cementoSol.validarStock(cantidad);
+            case "cemento apu":
+                return cementoApu.validarStock(cantidad);
+            case "cemento holcim":
+                return cementoHolcim.validarStock(cantidad);
+            default:
+                System.out.println("Producto no disponible");
+                return false;
+        }
+    }
+    
+    
+    
+    public double obtenerPrecio(String producto){
+        switch (producto.toLowerCase()) {
+            case "cemento sol":
+                return cementoSol.getPrecio();
+            case "cemento apu":
+                return cementoApu.getPrecio();
+            case "cemento holcim":
+                return cementoHolcim.getPrecio();
+            default:
+                System.out.println("Producto no valido");
+                return 0;
+        }
+    }
     
 }
